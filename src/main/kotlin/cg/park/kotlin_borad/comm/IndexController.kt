fc0e2dd1a2e1d5485c48f90fb2e27981a78dc187
pcg0902 <qkrcksrbs8@naver.com>
2025-09-23 10:41:46 +0900

    index 페이지 이동 구현

--- src/main/kotlin/cg/park/kotlin_borad/comm/IndexController.kt
+++ src/main/kotlin/cg/park/kotlin_borad/comm/IndexController.kt
@@ -1,14 +1,21 @@
 package cg.park.kotlin_borad.comm
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.ResponseBody
 
-@RestController
+@Controller
 class IndexController {
 
     @GetMapping("/test")
+    @ResponseBody
     fun defaultPage(str: String? = ""): String {
         return "welcome :$str";
+    }
+
+    @GetMapping("")
+    fun index(): String {
+        return "index";
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>테스트 페이지</title>
+</head>
+<body>
+<h1>안녕하세요</h1>
+</body>
+</html>
