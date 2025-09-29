fe1c0ef826de228eed7537dd4cea7e811a4dd88f
pcg0902 <qkrcksrbs8@naver.com>
2025-09-29 11:44:56 +0900

    Param 클래스에 default 성공,실패 응답 생성

--- src/main/kotlin/cg/park/kotlin_borad/util/Param.kt
+++ src/main/kotlin/cg/park/kotlin_borad/util/Param.kt
@@ -17,4 +17,34 @@ class Param() : HashMap<String, Any>() {
     fun set(key: String, value: Any) {
         put(key, value)
     }
+
+    fun success() : Param {
+        put("status", "200")
+        put("code", "S0001")
+        put("message", "Success")
+        return this
+    }
+    fun success(value: Any) : Param {
+        put("status", "200")
+        put("code", "S0001")
+        put("message", "Success")
+        put("data", value)
+        return this
+    }
+
+    fun fail() : Param {
+        put("status", "400")
+        put("code", "F0001")
+        put("message", "Fail")
+        return this
+    }
+
+    fun fail(value: Any) : Param {
+        put("status", "400")
+        put("code", "F0001")
+        put("message", "Fail")
+        put("data", value)
+        return this
+    }
+
 }
